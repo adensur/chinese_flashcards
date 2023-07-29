20a913e9e67a10c98ceeb81fdfd025b0644590c7
mgaiduk <mgaiduk@sharechat.co>
2023-07-29 12:59:16 +0100

    remove unused adaptive keyboard code

--- Flashcards App/Views/SuggestView.swift
+++ Flashcards App/Views/SuggestView.swift
@@ -6,7 +6,6 @@
 //
 
 import SwiftUI
-import Combine
 
 extension String {
     func hasUnicodePrefx(_ prefix: String) -> Bool {
@@ -29,50 +28,6 @@ extension String {
         
         // If we reach this point, the prefix is a valid prefix of the string
         return true
-    }
-}
-
-extension Publishers {
-    // 1.
-    static var keyboardHeight: AnyPublisher<CGFloat, Never> {
-        // 2.
-        let willShow = NotificationCenter.default.publisher(for: UIApplication.keyboardWillShowNotification)
-            .map {
-                print("willShow! height: \($0.keyboardHeight)")
-                return $0.keyboardHeight
-            }
-        
-        let willHide = NotificationCenter.default.publisher(for: UIApplication.keyboardWillHideNotification)
-            .map { _ in CGFloat(0) }
-        
-        // 3.
-        return MergeMany(willShow, willHide)
-            .eraseToAnyPublisher()
-    }
-}
-
-extension Notification {
-    var keyboardHeight: CGFloat {
-        return (userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? CGRect)?.height ?? 0
-    }
-}
-
-struct KeyboardAdaptive: ViewModifier {
-    @State private var keyboardHeight: CGFloat = 0
-
-    func body(content: Content) -> some View {
-        content
-            .padding(.bottom, keyboardHeight)
-            .onReceive(Publishers.keyboardHeight) {
-                self.keyboardHeight = $0
-                print("Keyboard height: \(keyboardHeight)")
-            }
-    }
-}
-
-extension View {
-    func keyboardAdaptive() -> some View {
-        ModifiedContent(content: self, modifier: KeyboardAdaptive())
     }
 }
 
