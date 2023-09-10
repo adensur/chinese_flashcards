6b0437e7f30e76aa4feecca3628cf9f035ca84bd
adensur <adensur@gmail.com>
2023-09-10 10:37:27 +0100

    Add custom button style for difficulty buttons

--- Flashcards App/Views/DifficultyButtonsView.swift
+++ Flashcards App/Views/DifficultyButtonsView.swift
@@ -23,19 +23,44 @@ struct AdvancedDifficultyButtonsView: View {
     }
 }
 
+private struct CustomButtonStyle: ButtonStyle {
+    var pressedColor: Color
+    @ViewBuilder
+    func makeBody(configuration: Configuration) -> some View {
+        let background = configuration.isPressed ? AnyView(pressedColor) : AnyView(EmptyView())
+        configuration.label
+            .background(background)
+            .cornerRadius(8)
+    }
+}
+
 struct SimpleDifficultyButtonsView: View {
     @ObservedObject var card: Card
     var callback: (_: Difficulty) -> Void
+    @State private var okTapped = false
     var body: some View {
         HStack {
             Spacer()
-            Button("nah") {
+            Button {
                 callback(Difficulty.Hard)
+            } label: {
+                Text("nah")
+                    .padding()
+                    .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .bottom)
+                    .contentShape(Rectangle())
             }
+            .buttonStyle(CustomButtonStyle(pressedColor: .red))
             Spacer()
-            Button("ok") {
+            Button {
+                okTapped = false
                 callback(Difficulty.Good)
+            } label: {
+                Text("ok")
+                    .padding()
+                    .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .bottom)
+                    .contentShape(Rectangle())
             }
+            .buttonStyle(CustomButtonStyle(pressedColor: .green))
             Spacer()
         }
     }
