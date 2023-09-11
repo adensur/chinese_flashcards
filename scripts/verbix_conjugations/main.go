96e58a922b57a81175a065a04b97462a1c629398
mgaiduk <mgaiduk@sharechat.co>
2023-09-11 12:09:01 +0100

    script to scrape verbix.com to get verb conjugations

---
+++ scripts/verbix_conjugations/main.go
@@ -0,0 +1,182 @@
+package main
+
+import (
+	"bufio"
+	"context"
+	"encoding/json"
+	"flag"
+	"fmt"
+	"log"
+	"net/url"
+	"os"
+	"strings"
+	"time"
+
+	"github.com/chromedp/chromedp"
+	"golang.org/x/net/html"
+)
+
+var (
+	input = flag.String("input", "", "Input file")
+)
+
+type MyNode struct {
+	Attrs map[string]string
+}
+
+func NewNode(n *html.Node) MyNode {
+	m := MyNode{}
+	m.Attrs = make(map[string]string)
+	for _, attr := range n.Attr {
+		m.Attrs[attr.Key] = attr.Val
+	}
+	return m
+}
+
+var gender = ""
+var isDevanagari = false
+var pronoun = ""
+var tense = ""
+var tense2 = ""
+
+type Result struct {
+	// gender -> perfective/imperfective -> tense -> pronoun -> form
+	Data map[string]map[string]map[string]map[string]string
+}
+
+var result Result
+
+func Process(n *html.Node, prefix string) {
+	node := NewNode(n)
+	if n.Type == html.ElementNode {
+		// fmt.Printf("Data: %v, prefix: %v\n", n.Data, prefix)
+	}
+	if n.Data == "Masculine forms" && strings.HasSuffix(prefix, "/h2") {
+		// fmt.Printf("Data: %v, prefix: %v\n", n.Data, prefix)
+		gender = "masculine"
+	}
+	if n.Data == "table" && node.Attrs["class"] == "verbtense" {
+		isDevanagari = true
+	}
+	if n.Data == "table" && node.Attrs["class"] == "verbtense nospeech" {
+		isDevanagari = false
+	}
+	if n.Data == "Feminine forms" && strings.HasSuffix(prefix, "/h2") {
+		// fmt.Printf("Data: %v, prefix: %v\n", n.Data, prefix)
+		gender = "feminine"
+	}
+	if node.Attrs["class"] == "pronoun" && isDevanagari {
+		// fmt.Printf("Data: %v, prefix: %v, attrs: %v\n", n.Data, prefix, node.Attrs)
+		pronoun = n.FirstChild.Data
+	}
+	if n.Data == "h4" {
+		// fmt.Printf("h4 Data: %v, prefix: %v, attrs: %v, first child: %s\n", n.Data, prefix, node.Attrs, n.FirstChild.Data)
+		tense = n.FirstChild.Data
+	}
+	if n.Data == "h3" {
+		// fmt.Printf("h4 Data: %v, prefix: %v, attrs: %v, first child: %s\n", n.Data, prefix, node.Attrs, n.FirstChild.Data)
+		tense2 = n.FirstChild.Data
+	}
+	_, ok := node.Attrs["data-speech"]
+	if ok && isDevanagari {
+		form := n.FirstChild.Data
+		if result.Data == nil {
+			result.Data = make(map[string]map[string]map[string]map[string]string)
+		}
+		if result.Data[gender] == nil {
+			result.Data[gender] = make(map[string]map[string]map[string]string)
+		}
+		if result.Data[gender][tense2] == nil {
+			result.Data[gender][tense2] = make(map[string]map[string]string)
+		}
+		if result.Data[gender][tense2][tense] == nil {
+			result.Data[gender][tense2][tense] = make(map[string]string)
+		}
+		result.Data[gender][tense2][tense][pronoun] = form
+		// fmt.Printf("Got form: %s, pronoun: %s, gender: %s tense: %s, tense2: %s\n", form, pronoun, gender, tense, tense2)
+	}
+	idx := 0
+	for c := n.FirstChild; c != nil; c = c.NextSibling {
+		Process(c, fmt.Sprintf("%s[%v]/%s", prefix, idx, n.Data))
+		idx += 1
+	}
+}
+
+func getConjugations(word string) error {
+	encodedWord := url.QueryEscape(word)
+	url := "https://www.verbix.com/webverbix/go.php?T1=" + encodedWord + "&Submit=Go&D1=47&H1=147"
+	ctx, cancel := chromedp.NewContext(context.Background())
+	defer cancel()
+
+	// Create a timeout for the operation
+	ctx, cancel = context.WithTimeout(ctx, 15*time.Second)
+	defer cancel()
+
+	var page string
+
+	// Navigate to the page, wait for it to load, execute some JavaScript and retrieve the HTML
+	err := chromedp.Run(ctx,
+		chromedp.Navigate(url),
+		// You can put a suitable selector that waits until some JavaScript-loaded elements are present.
+		chromedp.WaitVisible(`body`, chromedp.ByQuery),
+		chromedp.Evaluate(`document.body.innerHTML`, &page),
+	)
+	if err != nil {
+		log.Fatalf("Failed getting body HTML: %v", err)
+	}
+
+	doc, err := html.Parse(strings.NewReader(page))
+	if err != nil {
+		panic(err)
+	}
+	Process(doc, "/")
+	return nil
+}
+
+func main() {
+	flag.Parse()
+	if len(*input) == 0 {
+		fmt.Printf("Expected input file!\n")
+		panic("Expected input file!")
+	}
+
+	r, err := os.Open(*input)
+	if err != nil {
+		panic(err)
+	}
+	defer r.Close()
+	scanner := bufio.NewScanner(r)
+	for scanner.Scan() {
+		line := scanner.Text()
+		values := strings.Split(line, "\t")
+		word := values[0]
+		fmt.Fprintf(os.Stderr, "Processing word: %v\n", word)
+		expBackoff := 1 * time.Second
+		for {
+			result = Result{make(map[string]map[string]map[string]map[string]string)}
+			err = getConjugations(word)
+			if err == nil {
+				break
+			} else if err.Error() == "transient" {
+				fmt.Fprintf(os.Stderr, "Transient error, retrying word: %v\n", word)
+				fmt.Fprint(os.Stderr, "Sleeping for: ", expBackoff, "\n")
+				time.Sleep(expBackoff)
+				expBackoff *= 2
+				continue
+			} else {
+				panic(err)
+			}
+		}
+		js, err := json.Marshal(result)
+		if err != nil {
+			panic(err)
+		}
+		fmt.Printf("%s\n", js)
+	}
+
+	flag.Parse()
+	if len(*input) == 0 {
+		fmt.Printf("Expected input file!\n")
+		panic("Expected input file!")
+	}
+}
