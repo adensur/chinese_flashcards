c3a6fd133a1c30f26ff8e39f8a06231b5ad18406
mgaiduk <mgaiduk@sharechat.co>
2023-10-16 08:38:29 +0100

    add full hindi 235k translations

---
+++ Flashcards App/Views/CardViews/ScribblingExerciseView.swift
@@ -0,0 +1,18 @@
+//
+//  ScribblingExerciseView.swift
+//  Flashcards App
+//
+//  Created by Maksim Gaiduk on 14/10/2023.
+//
+
+import SwiftUI
+
+struct ScribblingExerciseView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ScribblingExerciseView()
+}
